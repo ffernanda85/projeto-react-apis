9151f2192d60854ea93e632f7d0be4d3eea3e693
ffernanda85 <f.fernanda85@gmail.com>
2023-04-26 00:13:08 -0300

    ajustes desing pagina detalhes

--- projeto-react-apis-app/src/page/detailsPage/DetailsPage.jsx
+++ projeto-react-apis-app/src/page/detailsPage/DetailsPage.jsx
@@ -2,6 +2,7 @@ import { Header } from "../../components/header/Header";
 import { Modal } from "../../components/modal/Modal";
 import * as s from "./styledDetails"
 import BG from "../../assets/img/cards/pngwing2.svg"
+import Poke from "../../assets/img/cards/pokemon/poke1.svg"
 import { Container } from "../../Container";
 
 export function DetailsPage() {
@@ -13,13 +14,43 @@ export function DetailsPage() {
                 <s.Title>Detalhes</s.Title>
 
                 <s.ContainerCardDetail >
+                    <s.ContainerBoxOne>
+                        <s.ContainerImgPokemon>
+                            <s.BoxImgFront> <s.ImgFront src="" alt="" /> </s.BoxImgFront>
+                            <s.BoxImgBack> <s.ImgBack src="" alt="" /> </s.BoxImgBack>
+                        </s.ContainerImgPokemon>
 
-                    <div>
-                        <div></div>
-                        <div></div>
-                    </div>
+                        <s.ContainerBaseStats>
+                            <s.TitleBaseStats>Base stats</s.TitleBaseStats>
+                            <s.BoxStats>
+                                <p>HP</p>
+                                <p>Attack</p>
+                                <p>Defense</p>
+                                <p>Sp. Atk</p>
+                                <p>Sp. Def</p>
+                                <p>Speed</p>
+                                <p>Total</p>
+                            </s.BoxStats>
+                        </s.ContainerBaseStats>
+                    </s.ContainerBoxOne>
+                   
+                    <s.ContainerBoxTwo>
 
-                    
+                        <s.ContainerBox>
+                            <s.BoxAbilites>
+                                <s.Id>Id</s.Id>
+                                <s.NamePokemon>Nome</s.NamePokemon>
+                                <div></div>
+                            </s.BoxAbilites>
+                            <s.BoxMoves></s.BoxMoves>
+                        </s.ContainerBox>
+
+                        <s.ContainerPokemon>
+                            <s.ImgPokemon src={Poke} alt="" />
+                        </s.ContainerPokemon>
+
+                    </s.ContainerBoxTwo>
+
                 </s.ContainerCardDetail>
 
             </s.Container>
--- projeto-react-apis-app/src/page/detailsPage/styledDetails.jsx
+++ projeto-react-apis-app/src/page/detailsPage/styledDetails.jsx
@@ -8,7 +8,6 @@ export const Container = styled.div`
     background: url(${({ BG }) => BG}) no-repeat 35% center  ;
     background-size: 908.99px 908.99px;
 `
-
 export const Title = styled.h1`
     font-family: 'Poppins';
     font-style: normal;
@@ -23,13 +22,111 @@ export const ContainerCardDetail = styled.div`
     border-radius: 37.8857px;
     width: 1389px;
     height: 663px;
+    display: flex;
+    //justify-content: center;
+    position: relative;
+    //gap: 2rem;
 `
-export const BackGroundImgContainer = styled.img`
-    position: absolute;
-    width: 908.99px;
-    height: 908.99px;
-    z-index: -1;
+export const ContainerBoxOne = styled.div`
+    width: 50%;
+    display: flex;
+    gap: 2rem;
+    //justify-content: flex-start;
 `
-export const BackGroundImgCard = styled.img`
+export const ContainerImgPokemon = styled.div`
+   display: flex;
+   flex-direction: column;
+   justify-content: space-evenly;
+   align-items: center;
+   gap: 1.2rem;
+`
+export const BoxImgFront = styled.div`
+    width: 282px;
+    height: 282px;
+    border-radius: 8px;
+    background-color: #ffff;
+`
+export const BoxImgBack = styled.div`
+    width: 282px;
+    height: 282px;
+    border-radius: 8px;
+    background-color: #ffff;
+`
+export const ImgFront = styled.img`
     
+`
+export const ImgBack = styled.img`
+    
+`
+export const ContainerBoxTwo = styled.div`
+    width: 50%;
+    display: flex;
+    gap: 2rem;
+    //justify-content: space-evenly;
+`
+export const ContainerBaseStats = styled.div`
+    width: 343px;
+    height: 613px;
+    background: #FFFFFF;
+    border-radius: 12px;
+    align-self: center;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 1.1875rem;
+`
+export const TitleBaseStats = styled.h3`
+    font-family: 'Inter';
+    font-weight: 800;
+    font-size: 1.5rem;
+    line-height: 1.8125rem;
+    color: #000000;
+`
+export const BoxStats = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: .8rem;
+    font-family: 'Inter';
+`
+export const ContainerBox = styled.div`
+    //width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    gap: 1.2rem;
+`
+export const BoxMoves = styled.div`
+    width: 292px;
+    height: 453px;
+    background: #FFFFFF;
+    border-radius: 8px;
+`
+export const Id = styled.p`
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1rem;
+    line-height: 1.1875rem;
+    color: #FFFFFF;
+`
+export const NamePokemon = styled.h2`
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 3rem;
+    line-height: 3.625rem;
+    color: #FFFFFF;
+`
+export const BoxAbilites = styled.div`
+    
+`
+export const ContainerPokemon = styled.div`
+   
+    
+`
+export const ImgPokemon = styled.img`
+    position: absolute;
+    right: 35.14px;
+    bottom: 520px;
+    width: 270px;
 `
